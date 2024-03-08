e2feafac554e9ab17564e420209039977ba64a52
Ayush Panchal <ayushrockz11@gmail.com>
2024-03-08 13:28:13 +0530

    added env vars for hosting

--- client/src/services/httpAxios.js
+++ client/src/services/httpAxios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const BACKEND_ADDRESS = process.env.REACT_APP_BACKEND_ADDRESS
+
 export const httpAxios = axios.create({
-  baseURL: "https://internshup-portal-backend.onrender.com",
+  baseURL: BACKEND_ADDRESS,
   withCredentials: "true",
 });
 
