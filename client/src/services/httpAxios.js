eb0fd9c00c1097a84a3d3dc34973f911e9b5802f
Ayush Panchal <ayushrockz11@gmail.com>
2025-02-06 22:05:10 +0530

    yes

--- client/src/services/httpAxios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const BACKEND_ADDRESS = process.env.REACT_APP_BACKEND_ADDRESS
-
-export const httpAxios = axios.create({
-  baseURL: BACKEND_ADDRESS,
-  withCredentials: "true",
-});
-
